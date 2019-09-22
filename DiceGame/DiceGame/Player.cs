e4f51114bb433d1d76f7e080833ed8d4fdc404ae
retr0web <45012672+retr0web@users.noreply.github.com>
2019-09-22 18:46:26 +0300

    Add files via upload

---
+++ DiceGame/DiceGame/Player.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace DiceGame
+{
+    class Player
+    {
+        private string name;
+
+        private int wins;
+
+        private int sum;
+        
+        // PROPERTIES
+        public string Name { get; set; }
+        public int Wins { get; set; }
+        public int Sum { get; set; }
+    }
+}
--- DiceGame/DiceGame/Program.cs
+++ DiceGame/DiceGame/Program.cs
@@ -15,60 +15,219 @@ namespace DiceGame
     {
         static void Main(string[] args)
         {
-            Greetings();
-        }
+            Player playerOne = new Player();
+            Player playerTwo = new Player();
+            Player computer = new Player();
+            computer.Name = "Bobby Shmurda";
+            bool twoPlayers = false;
+            
+            
+            Rules();
+            HowManyPlayers();
+            
+            // explanation of the game
+            void Rules()
+            {
+                Console.WriteLine("This is a simple ancient game called Dice");
+                Console.WriteLine("Ok, now how it goes. You both roll 2 dices and the one who scores more wins!");
+                Console.WriteLine(
+                    "So after you rolled dices you get 2 random numbers (from 1 to 6) and the sum of them.");
+                Console.WriteLine("And who got bigger sum wins a round");
+                Console.WriteLine("You and your friend can have multiple rounds");
+                Console.WriteLine("///////////////////////////////////////////////////////");
+            }
+            
+            // decides to play coop or single player
+            void HowManyPlayers()
+            { 
+                Console.WriteLine("///////////////////////////////////////////////////////");
+                Console.WriteLine("Greetings. How many players will play");
+               int numberOfPlayers = Convert.ToInt32(Console.ReadLine());
 
-        // Choose with who you will play: Computer or a real person
-        static void Greetings()
-        {
-            Console.WriteLine("Greetings, traveller! Did you brought a friend with you?");
-            Console.WriteLine("Hint: answer must be yes or no");
-            Console.WriteLine("Additional hint: if you want to exit, just type exit");
-            
-            string travellerAnswer = Console.ReadLine();
-            
-            if (travellerAnswer == "yes")
-            {
-                PlayerBroughtFriend();
-            }
-            
-            else if (travellerAnswer == "no")
-            {
-                PlayerCameAlone();
-            }
-            
-            else if (travellerAnswer == "exit")
-            {
-                ExitGame();
+               if (numberOfPlayers == 2)
+               {
+                   twoPlayers = true;
+                   Introduce(playerOne.Name);
+                   Introduce(playerTwo.Name);
+                   GameForTwo();
+               }
+
+               else if (numberOfPlayers == 1)
+               {
+                   Introduce(playerOne.Name);
+                   GameForOne();
+               }
+
+               else
+               {
+                   Console.WriteLine("You entered a wrong number. Please try again");
+                   HowManyPlayers();
+               }
             }
 
-            else
+            // giving a name to players
+            void Introduce(string player)
             {
-                Console.WriteLine("You typed wrong answer! Let's try again");
-                Greetings();
+                Console.WriteLine("///////////////////////////////////////////////////////");
+                Console.WriteLine("Player, enter your name");
+                string nameOfPlayer = Console.ReadLine();
+
+                if (nameOfPlayer == null)
+                {
+                    Console.WriteLine("You didn't enter a name. Let's try again");
+                }
+                
+                else
+                {
+                    player = nameOfPlayer;
+                    Console.WriteLine($"Greetings, {player}");  
+                }
+            }
+
+            // game with a person
+            void GameForTwo()
+            {
+                Console.WriteLine("///////////////////////////////////////////////////////");
+                Console.WriteLine("Now each player will role dices");
+                Console.WriteLine("To do so, type 'roll'");
+                string command = Console.ReadLine();
+
+                if (command == "roll")
+                {
+                    RollDice(playerOne.Sum);
+                    RollDice(playerTwo.Sum);
+                    Result(playerOne.Sum, playerTwo.Sum);
+                    AnotherRound();
+                }
+
+                else
+                {
+                    Console.WriteLine("Hahaha, you tried to hack the game");
+                    ExitGame();
+                }
+            }
+
+            // game with computer
+            void GameForOne()
+            {
+                Console.WriteLine("///////////////////////////////////////////////////////");
+                Console.WriteLine("Now each player will role dices");
+                Console.WriteLine("You will be playing with computer");
+                Console.WriteLine("To do so, type 'roll'");
+                string command = Console.ReadLine();
+
+                if (command == "roll")
+                {
+                    RollDice(playerOne.Sum);
+                    RollDice(computer.Sum);
+                    Result(playerOne.Sum, computer.Sum);
+                    AnotherRound();
+                }
+
+                else
+                {
+                    Console.WriteLine("Hahaha, you tried to hack the game");
+                    ExitGame();
+                }
+            }
+
+            // creates 2 random numbers and sum them
+            void RollDice(int player)
+            {
+                Console.WriteLine("///////////////////////////////////////////////////////");
+                Random rnd = new Random();
+                int diceOne = rnd.Next(1, 7);
+                int diceTwo = rnd.Next(1, 7);
+                int score = diceOne + diceTwo;
+                player = score;
+                Console.WriteLine($"Your score is {player}");
+            }
+
+            // checks who's got a bigger score
+            void Result(int firstSum, int secondSum)
+            {
+                if (firstSum > secondSum)
+                {
+                    Console.WriteLine("///////////////////////////////////////////////////////");
+                    Console.WriteLine($"{playerOne.Name} scored {playerOne.Sum}");
+                    Console.WriteLine($"{playerTwo.Name} scored {playerTwo.Sum}");
+                    Console.WriteLine($"The winner is {playerOne.Name}");
+                    playerOne.Wins += 1;
+                }
+                
+                else if (firstSum < secondSum)
+                {
+                    Console.WriteLine("///////////////////////////////////////////////////////");
+                    Console.WriteLine($"{playerOne.Name} scored {playerOne.Sum}");
+                    Console.WriteLine($"{playerTwo.Name} scored {playerTwo.Sum}");
+                    Console.WriteLine($"The winner is {playerTwo.Name}");
+                    playerTwo.Wins += 1;
+                }
+                
+                else if (firstSum == secondSum)
+                {
+                    Console.WriteLine("///////////////////////////////////////////////////////");
+                    Console.WriteLine($"{playerOne.Name} scored {playerOne.Sum}");
+                    Console.WriteLine($"{playerTwo.Name} scored {playerTwo.Sum}");
+                    Console.WriteLine($"DRAW");
+                    playerOne.Wins += 1;
+                    playerTwo.Wins += 1;
+                }
+            }
+
+            void AnotherRound()
+            {
+                Console.WriteLine("///////////////////////////////////////////////////////");
+                Console.WriteLine("Would you like to play another round or see leaderboard?");
+                string game = Console.ReadLine();
+
+                if (game == "another round" && twoPlayers == true)
+                {
+                    GameForTwo();
+                }
+
+                if (game == "another round" && twoPlayers == false)
+                {
+                    GameForOne();
+                }
+
+                else if (game == "leaderboard" && twoPlayers == true)
+                {
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine($"{playerOne.Name} won {playerOne.Wins} times");
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine($"{playerTwo.Name} won {playerTwo.Wins} times");
+                    Console.WriteLine("-----------------------");
+                    AnotherRound();
+                }
+                
+                else if (game == "leaderboard" && twoPlayers == false)
+                {
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine($"{playerOne.Name} won {playerOne.Wins} times");
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine($"{computer.Name} won {computer.Wins} times");
+                    Console.WriteLine("-----------------------");
+                    AnotherRound();
+                }
+                
+                else if (game == "exit")
+                {
+                    ExitGame();
+                }
+
+                else
+                {
+                    ExitGame();
+                }
+            }
+            
+            // Stopping execution of game
+            void ExitGame()
+            {
+                Console.WriteLine("Your game has ended! Press enter to confirm exit");
+                Console.ReadLine();
             }
         }
-        
-        // Play with real person
-        static void PlayerBroughtFriend()
-        {
-            Console.WriteLine("You Brought a friend");
-            Console.ReadLine();
-        }
-        
-        // Single player
-        static void PlayerCameAlone()
-        {
-            Console.WriteLine("Don't worry, we will find you buddy to play with!");
-            Console.ReadLine();
-        }
-        
-        // Stopping execution of game
-        static void ExitGame()
-        {
-            Console.WriteLine("Your game has ended! Press enter to confirm exit");
-            Console.ReadLine();
-        }
-            
     }
 }
